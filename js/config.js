8473c60c9bd93feaeb137b1916251416b18cb1ff
Praajaktaaa <prajakta.d.patankar@gmail.com>
2025-05-03 20:46:29 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,8 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_WY0clb7dS', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '7vaimaboietqbpmgcgsgfpl00p', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        region: 'us-east-1', // e.g. us-east-2
+        userPoolWebClientId: 'us-east-1_WY0clb7dS'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
